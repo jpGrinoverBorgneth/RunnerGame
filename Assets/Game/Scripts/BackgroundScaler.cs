615954edd93e6d1920afef658627d722d2bad05f
jaypi <jp.borgneth@gmail.com>
2023-09-24 16:21:16 -0300

    first setup of project and initial components

---
+++ Assets/Game/Scripts/BackgroundScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    private Vector3 _startScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startScale = transform.localScale;
+
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        transform.localScale = new Vector3(width, height, _startScale.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/BackgroundTextureAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTextureAnimator : MonoBehaviour
+{
+    private Material _material;
+    private Vector2 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = gameObject.GetComponent<Renderer>().material;
+        _offset = _material.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _offset.x = _offset.x + (0.2f * Time.deltaTime);
+        _material.SetTextureOffset("_MainTex", _offset);
+    }
+}
